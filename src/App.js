cbede5799e66246381415dc7f1fe2140c8859d2b
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-04 18:44:59 +0300

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
   return (
     <>
