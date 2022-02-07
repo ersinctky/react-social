cb086d675559aaeec34c9eb1cb368439fa91e15e
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-07 13:24:58 +0300

    src/apiCalls.js created

---
+++ src/apiCalls.js
@@ -0,0 +1,10 @@
+import myApi from "./api/myApi";
+export const loginCall = async (userCredential, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await myApi.post("/auth/login", userCredential);
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAILURE", payload: err });
+  }
+};
