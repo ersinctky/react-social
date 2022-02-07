2f1f619b1317b0e0a7fa647ef3f3f43ba1a125fc
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-07 12:47:38 +0300

    handleClick func  added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
+import { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+   const email = useRef()
+   const password = useRef()
+
+  const handleClick = (e)=>{
+    e.preventDefault()
+  }
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +19,15 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" className="loginInput" />
+            <input placeholder="Password" type="password" className="loginInput" />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
