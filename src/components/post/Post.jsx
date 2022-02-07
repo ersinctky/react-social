3e354305cbc03009f249c8ea69dc8c1c8e5bd24c
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-07 14:50:36 +0300

    src/components post like & dislike functionality

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,21 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { myApi } from "../../api/myApi";
 import {format } from "timeago.js"
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({ post }) {
   const [like,setLike] = useState(post.likes.length)
   const [isLiked,setIsLiked] = useState(false)
   const [user,setUser]=useState({})
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user: currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    setIsLiked(post.likes.includes(currentUser._id));
+  }, [currentUser._id, post.likes]);
 
 
   useEffect(() => {
@@ -23,10 +29,13 @@ export default function Post({ post }) {
     
    }, [post.userId]);
 
-  const likeHandler =()=>{
-    setLike(isLiked ? like-1 : like+1)
-    setIsLiked(!isLiked)
-  }
+   const likeHandler = () => {
+    try {
+      myApi.put("/posts/" + post._id + "/like", { userId: currentUser._id });
+    } catch (err) {}
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
@@ -35,7 +44,11 @@ export default function Post({ post }) {
             <Link to={`/profile/${user.username}`}>
             <img
               className="postProfileImg"
-              src={user.profilePicture || PF+"person/noAvatar.png"}
+              src={
+                user.profilePicture
+                  ? PF + user.profilePicture
+                  : PF + "person/noAvatar.png"
+              }
               alt=""
             />
             </Link>
