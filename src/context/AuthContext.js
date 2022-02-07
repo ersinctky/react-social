65d6e59921226f3c34704fe4fe1a3552f0c59616
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-07 13:05:16 +0300

    src/context/AuthContext.js AuthContextProvider added

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { AuthReducer } from "./AuthReducer";
 
 const INITIAL_STATE = {
   user: null,
@@ -7,3 +8,24 @@ const INITIAL_STATE = {
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
