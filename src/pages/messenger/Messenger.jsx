aac961a660bab7cb07a26a523f680a33b194d2ee
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-08 19:43:37 +0300

    src/components/conversations created

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -1,7 +1,25 @@
+import React from "react"
+import Topbar from "../../components/topbar/Topbar";
 import "./messenger.css";
 
 export default function Messenger() {
-  return <div className="messenger">
-messenger
-  </div>;
+  return (
+      <>
+      <Topbar />
+      <div className="messenger">
+       <div className="chatMenu">
+           <div className="chatMenuWrapper">
+           <input placeholder="Search for friends" className="chatMenuInput" />
+           </div>
+       </div>
+       <div className="chatBox">
+           <div className="chatBoxWrapper"></div>
+       </div>
+       <div className="chatOnline">
+           <div className="chatOnlineWrapper"></div>
+       </div>
+      </div>;
+      </>
+  )
+  
 }
